e7957a15c923b7038011c64df4d1e7dd4bd06d23
Breno Santin <brenolimaa96@gmail.com>
2022-09-12 16:15:10 -0300

    modified packagejson

--- dist/helpers/associations.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Factura_1 = require("../models/Factura");
-const Obra_1 = require("../models/Obra");
-const Provedor_1 = require("../models/Provedor");
-Provedor_1.Provedor.hasMany(Factura_1.Factura);
-Factura_1.Factura.belongsTo(Provedor_1.Provedor);
-Obra_1.Obra.hasMany(Factura_1.Factura);
-Factura_1.Factura.belongsTo(Obra_1.Obra);
